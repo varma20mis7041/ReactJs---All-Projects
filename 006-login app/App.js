892d42557767c8c8cf4b66bd08dcd1b93476c73b
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-16 13:07:37 +0530

    Add files via upload

---
+++ 006-login app/App.js
@@ -0,0 +1,17 @@
+import Home from './components/Home'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Home />
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 29 jul 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
