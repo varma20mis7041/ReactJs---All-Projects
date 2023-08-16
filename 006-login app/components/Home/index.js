4165b6b6a1c4c0fc047b31662665dcc8cc13648f
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-16 13:09:30 +0530

    Update index.js

--- 006-login app/components/Home/index.js
+++ 006-login app/components/Home/index.js
@@ -1,1 +1,41 @@
+// Write your code here
 
+import {Component} from 'react'
+
+import Welcome from '../Message/index'
+
+import Login from '../Login/index'
+
+import Logout from '../Logout/index'
+
+import './index.css'
+
+class Home extends Component {
+  state = {login: false}
+
+  changeButton = () => {
+    this.setState(prevState => ({login: !prevState.login}))
+  }
+
+  render() {
+    const {login} = this.state
+    console.log(login)
+    let logInLogoutMessage
+    if (login === false) {
+      logInLogoutMessage = <Login login={this.changeButton} />
+    } else {
+      logInLogoutMessage = <Logout logout={this.changeButton} />
+    }
+
+    return (
+      <div className="bg_container">
+        <div className="login_logout_page_card_container">
+          <Welcome login={login} />
+          {logInLogoutMessage}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
