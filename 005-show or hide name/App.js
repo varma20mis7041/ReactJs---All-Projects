7969788bb3c88b8bea3cfa33243bab0add7b7fed
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-16 12:45:52 +0530

    Add files via upload

---
+++ 005-show or hide name/App.js
@@ -0,0 +1,17 @@
+import ShowHide from './components/ShowHide'
+
+import './App.css'
+
+const App = () => (
+  <div>
+    <ShowHide />
+    <div className="last-container">
+      <p>
+        Developed by Bhargav varma | Developed date : 28 jul 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </div>
+)
+
+export default App
