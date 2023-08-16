b0b5188aa794447645123821a0a456e4c2fde4de
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-16 18:13:06 +0530

    Add files via upload

---
+++ 009-Simple todo list/App.js
@@ -0,0 +1,18 @@
+import SimpleTodos from './components/SimpleTodos'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <SimpleTodos />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 30 jul 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
