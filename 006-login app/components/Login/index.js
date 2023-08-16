19b54e0e6c103d1de5efa910e82aaa1f9c178372
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-16 13:15:22 +0530

    Create index.js

---
+++ 006-login app/components/Login/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+
+import './index.css'
+
+const Login = props => {
+  const {login} = props
+  return (
+    <button className="login_button" type="button" onClick={login}>
+      Login
+    </button>
+  )
+}
+
+export default Login
