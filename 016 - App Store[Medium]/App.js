5e965f4b2f9729aca2c5b8b6c3c7d5166c8209c6
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-17 13:03:21 +0530

    Create App.js

---
+++ 016 - App Store[Medium]/App.js
@@ -0,0 +1,18 @@
+import AppStore from './components/AppStore'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <AppStore />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 12 Aug 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
