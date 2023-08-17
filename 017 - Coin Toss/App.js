95dc5d03c63939af993d3dba252534c317b16b95
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-17 13:52:13 +0530

    Create App.js

---
+++ 017 - Coin Toss/App.js
@@ -0,0 +1,18 @@
+import CoinToss from './components/CoinToss'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <CoinToss />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 13 Aug 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
