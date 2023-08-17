8a82529a3048ca80b52dca9eeab0e19e4ea7163b
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-17 12:52:32 +0530

    Create App.js

---
+++ 015 - Capitals App/App.js
@@ -0,0 +1,18 @@
+import Capitals from './components/Capitals'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Capitals />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 8 Aug 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
