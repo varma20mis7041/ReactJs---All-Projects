178b98fa66fe06cf143823b5a02086f3d409dfb9
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-17 12:29:42 +0530

    Create App.js

---
+++ 014-Gallery App/App.js
@@ -0,0 +1,18 @@
+import Gallery from './components/Gallery/index'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Gallery />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 7 Aug 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
