068f9c703f03a562a02c8ccbdb2bbdb54da6c939
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-19 20:35:43 +0530

    Create App.js

---
+++ 019-Comments App[Medium]/App.js
@@ -0,0 +1,18 @@
+import Comments from './components/Comments'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Comments />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 19 Aug 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
