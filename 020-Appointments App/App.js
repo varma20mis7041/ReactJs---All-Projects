0cf1d2bfda370e3a9e4cc00a4e1013e96abe3afb
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-20 11:27:52 +0530

    Create App.js

---
+++ 020-Appointments App/App.js
@@ -0,0 +1,18 @@
+import Appointments from './components/Appointments'
+
+import './App.css'
+
+const App = () => (
+  <div>
+    <Appointments />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 20 Aug 2023 | contact :
+        bhargav.coding@gmail.com
+      </p>
+    </div>
+  </div>
+)
+
+export default App
