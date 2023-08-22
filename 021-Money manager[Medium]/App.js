538b0f91b5d8216147980e5ea7ca064c37b53b06
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-22 21:56:49 +0530

    Create App.js

---
+++ 021-Money manager[Medium]/App.js
@@ -0,0 +1,18 @@
+import MoneyManager from './components/MoneyManager'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <MoneyManager />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 22 Aug 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
