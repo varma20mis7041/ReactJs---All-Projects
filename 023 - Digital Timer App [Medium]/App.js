ff3f3eb4ab0187a492ce47b3a235a3b9bfe71e92
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-28 23:43:30 +0530

    Update App.js

--- 023 - Digital Timer App [Medium]/App.js
+++ 023 - Digital Timer App [Medium]/App.js
@@ -2,6 +2,17 @@ import DigitalTimer from './components/DigitalTimer'
 
 import './App.css'
 
-const App = () => <DigitalTimer />
+const App = () => (
+  <div>
+    <DigitalTimer />
+
+    <div className="last-container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 27 Aug 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </div>
+)
 
 export default App
