cfbfcd694a50a5a85964c8c612e113e5725a6833
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-29 09:48:43 +0530

    Create App.js

---
+++ 024 - Stop watch/App.js
@@ -0,0 +1,18 @@
+import Stopwatch from './components/Stopwatch'
+
+import './App.css'
+
+const App = () => (
+  <div>
+    <Stopwatch />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 29 Aug 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </div>
+)
+
+export default App
