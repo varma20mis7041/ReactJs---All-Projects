5d155888d3e125b67b4270a0a62dbcf50c98fd8f
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-08-31 16:18:08 +0530

    Create App.js

---
+++ 026 - Password Manager[Assignment -2]/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+import PasswordManagerHome from './components/PasswordManagerHome/index'
+
+const App = () => (
+  <div>
+    <PasswordManagerHome />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | Developed date : 31 Aug 2023 | contact :
+        bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </div>
+)
+
+export default App
