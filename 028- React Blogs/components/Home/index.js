b53dfa99bac9a6b01505248c06926f3935ab59bd
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-09-19 22:33:08 +0530

    Create index.js

---
+++ 028- React Blogs/components/Home/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+// import {userInfo} from 'os'
+import UserInfo from '../UserInfo/index'
+import BlogList from '../BlogList/index'
+
+const Home = () => (
+  <div>
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
