3eb7a88d1f56e2b9c25c4fd374f78c8d9ac1a779
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-09-20 21:19:54 +0530

    Create App.js

---
+++ 029-Crypto Currency Tracker/App.js
@@ -0,0 +1,17 @@
+import CryptocurrencyTracker from './components/CryptocurrencyTracker'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <CryptocurrencyTracker />
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | Date of completion : 20 sep 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
