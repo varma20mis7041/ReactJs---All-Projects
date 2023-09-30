e889a936fba5bd4bedf2359cca1532885870b5ad
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-09-30 09:16:16 +0530

    Create app.js

---
+++ 031 - Form Validation in React/app.js
@@ -0,0 +1,17 @@
+import RegistrationForm from './components/RegistrationForm'
+import './App.css'
+
+const App = () => (
+  <>
+    <RegistrationForm />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | date of completion : 29 sep 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
