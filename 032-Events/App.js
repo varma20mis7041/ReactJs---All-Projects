172761158c0c6da0a9c76ee843727ff75345e8b8
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-10-07 17:50:05 +0530

    Create App.js

---
+++ 032-Events/App.js
@@ -0,0 +1,18 @@
+import Events from './components/Events'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Events />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | date of completion : 6 oct 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
