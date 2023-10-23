fe5feb594e61fc1b6a6db1005215f25fd4c4d7f2
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-10-23 22:27:02 +0530

    Create App.js

---
+++ 33-Github Popular Repos/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+import GithubPopularRepos from './components/GithubPopularRepos'
+
+const App = () => (
+  <>
+    <GithubPopularRepos />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | date of completion : 23 oct 2023 | contact
+        : bhargav.coding@gmail.com
+      </p>
+    </div>
+  </>
+)
+
+export default App
