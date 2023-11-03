3eeec25ab29e4cbf0805837e537959ae7b5e8697
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-11-03 09:53:52 +0530

    Create App.js

---
+++ 034 - Cowin dashboard using NPM/App.js
@@ -0,0 +1,18 @@
+import CowinDashboard from './components/CowinDashboard'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <CowinDashboard />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | date of completion : 3 Nov 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
