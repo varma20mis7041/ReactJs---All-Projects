3fb9a9863ebbcd37e487ec7f39d42854347246ef
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-11-21 09:55:05 +0530

    Create index.js

---
+++ 040-Layout Builder/components/Header/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+
+import './index.css'
+
+const Header = () => (
+  <div className="header_bg_container">
+    <h1 className="header_section_main_heading_element">Header</h1>
+  </div>
+)
+export default Header
