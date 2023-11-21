7158d53e0c268a905f69605fe6ae031098fbea62
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-11-21 09:55:48 +0530

    Create index.js

---
+++ 040-Layout Builder/components/Layout/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+import Header from '../Header'
+import Body from '../Body'
+import Footer from '../Footer'
+
+const Layout = () => (
+  <div className="layout_bg_container">
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+)
+export default Layout
