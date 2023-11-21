348522a881c2d0ae52c7bca3ce32ace2c37f639c
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-11-21 09:54:21 +0530

    Create index.js

---
+++ 040-Layout Builder/components/Footer/index.js
@@ -0,0 +1,9 @@
+// Write your code here
+import './index.css'
+
+const Footer = () => (
+  <div className="footer_bg_container">
+    <h1 className="footer_section_main_heading">Footer</h1>
+  </div>
+)
+export default Footer
