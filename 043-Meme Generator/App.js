e9a53c33fb2462f1b10a08732a5ae9085165ca52
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-12-12 18:36:49 +0530

    Create App.js

---
+++ 043-Meme Generator/App.js
@@ -0,0 +1,18 @@
+import MemeGenerator from './components/MemeGenerator'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <MemeGenerator />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | date of completion : 12 Dec 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
