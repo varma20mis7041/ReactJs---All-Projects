5f970951cdcaabba5984a987024206a921fa9c30
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-12-15 19:34:23 +0530

    Create App.js

---
+++ 045-Rock Paper Scissors [Final Assignment-1]/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import RockPaperScissors from './components/RockPaperScissors'
+
+const App = () => (
+  <>
+    <RockPaperScissors />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | Date of completion : 15 Dec 2023 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
