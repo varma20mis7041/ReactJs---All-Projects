01647c4f3d7d740dfc96e5b5c471e750b1cb1b00
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-12-15 19:29:04 +0530

    Create StyledComponents.js

---
+++ 045-Rock Paper Scissors [Final Assignment-1]/components/PlayingViewButton/StyledComponents.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const CustomImageElementInPlayingView = styled.img`
+  height: 250px;
+  width: 250px;
+  @media screen and (max-width: 768px) {
+    height: 170px;
+    width: 170px;
+  }
+`
+export const CustomPlayingViewButton = styled.button`
+  border-width: 0px;
+  padding: 0px;
+  background-color: transparent;
+  cursor: pointer;
+  margin-left: 15px;
+  margin-right: 15px;
+`
