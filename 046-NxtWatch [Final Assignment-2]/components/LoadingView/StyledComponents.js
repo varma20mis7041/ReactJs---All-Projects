f62dbc474daafc984dd1917a11ab5cfe25e9f575
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2024-01-05 11:10:37 +0530

    Create StyledComponents.js

---
+++ 046-NxtWatch [Final Assignment-2]/components/LoadingView/StyledComponents.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const CustomLoaderContainer = styled.div`
+  height: 100vh;
+  width: 82vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 767px) {
+    width: 100vw;
+  }
+`
+
+export default CustomLoaderContainer
