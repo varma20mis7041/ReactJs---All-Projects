880d60450bdc06fee9130cce83afaa0aa403e7f7
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2024-01-05 10:01:26 +0530

    Create index.js

---
+++ 046-NxtWatch [Final Assignment-2]/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <div className="last_container">
+        <p>
+          Developed by @Bhargav varma | Date of completion : 4 Jan 2024 |
+          contact : bhargav.coding@gmail.com{' '}
+        </p>
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
